405aa360a1d183eee7b8d9a1d3cf797b75e73194
TEDDDDGHCHCVBBCB <akdrjsdl102@naver.com>
2023-11-15 14:04:14 +0900

    메뉴창 구현중

--- matdori-react/src/components/restaurant/menu/MenuByRes.js
+++ matdori-react/src/components/restaurant/menu/MenuByRes.js
@@ -1,21 +1,60 @@
-import { useRecoilValue } from "recoil";
-import { busIdState } from "../../recoil";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import { useRecoilValue } from "recoil";
+// import { busIdState } from "../../../recoil";
 
 
 
-const BusProfile = () => {
-  const [busInfo, setBusInfo] = useState({});
+const MenuByRes = (props) => {
+  // const busId = useRecoilValuealue(busIdStateIdState);
+  const [restaurantList, setRestaurantList] = useState([]);
+  // const resNo = props.resNo; // 매장 번호는 props를 통해 전달되어야 합니다.
+  const [resNo, setResNo] = useState(''); // 매장 번호는 props를 통해 전달되어야 합니다.
 
-  const busId = useRecoilValue(busIdState);
+  useEffect(() => {
+    const loadRestaurant = () => {
+      const response =  axios({
+        url: `${process.env.REACT_APP_REST_API_URL}/menu/list/${resNo}`,
+        method: "get",
+        params: {
+          resNo : resNo
+        }
+      })
+      .then((response) => {
+        setRestaurantList(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+    };
 
- 
+    if (resNo) {
+      loadRestaurant();
+    }
+  }, []); // resNo가 변경될 때마다 요청
 
   return (
-    <>
-      <h1>{busInfo.busName}님 반갑습니다</h1>
-   
-    </>
+    <div className="container mt-5">
+      <h1>매장 메뉴 목록</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>메뉴 번호</th>
+            <th>메뉴 이름</th>
+            <th>메뉴 설명</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurantList.map((menu) => (
+            <tr key={menu.menuNo}>
+              <td>{menu.menuNo}</td>
+              <td>{menu.menuName}</td>
+              <td>{menu.menuContent}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
