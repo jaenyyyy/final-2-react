2ac468e7e3f1ea366dfa1dc97ef6209fd15c29d0
ph2tiger <ph2tiger@naver.com>
2023-11-15 18:30:47 +0900

    이메일 인증 기능구현

--- matdori-react/src/components/bus/FindId.js
+++ matdori-react/src/components/bus/FindId.js
@@ -1,34 +1,33 @@
-// FindId.js
-
 import React, { useState } from "react";
 import axios from 'axios';
 
 const FindId = () => {
   const [busRegNo, setBusRegNo] = useState("");
   const [busPw, setBusPw] = useState("");
-  const [foundId, setFoundId] = useState(null); // 아이디를 저장할 상태
+  const [foundId, setFoundId] = useState(null);
+  const [noMatch, setNoMatch] = useState(false);
 
   const handleFindId = (e) => {
     e.preventDefault();
   
-    // 입력된 정보를 기반으로 아이디를 찾기 위해 API 호출
     axios.post(`http://localhost:8080/business/findId/busregno/${busRegNo}/buspw/${busPw}`)
       .then((response) => {
-        // 아이디를 찾은 경우 상태에 설정
-        setFoundId(response.data); // 응답에 받은 아이디 설정
+        setFoundId(response.data);
+        setNoMatch(false);
       })
       .catch((error) => {
         console.error('아이디를 찾는데 실패했습니다:', error);
-        // 잘못된 정보 또는 서버 문제 등의 오류를 처리합니다.
+        setFoundId(null);
+        setNoMatch(true);
       });
   };
 
   return (
     <div className="container mt-5">
-      <h1>아이디 찾기 페이지</h1>
-      <form onSubmit={handleFindId}>
-        <div className="mb-3">
-          <label htmlFor="busRegNo" className="form-label">사업자 등록번호</label>
+      <h1 className="text-center mb-4">아이디 찾기</h1>
+      <form onSubmit={handleFindId} className="row g-3">
+        <div className="col-6">
+          <label htmlFor="busRegNo" className="form-label">회원가입시 입력한 사업자 등록번호</label>
           <input
             type="text"
             className="form-control"
@@ -36,10 +35,11 @@ const FindId = () => {
             value={busRegNo}
             onChange={(e) => setBusRegNo(e.target.value)}
             required
+            placeholder="10자리 숫자입니다"
           />
         </div>
 
-        <div className="mb-3">
+        <div className="col-6">
           <label htmlFor="busPw" className="form-label">비밀번호</label>
           <input
             type="password"
@@ -48,15 +48,24 @@ const FindId = () => {
             value={busPw}
             onChange={(e) => setBusPw(e.target.value)}
             required
+            placeholder="대,소문자,숫자,특수 문자를 포함하는 8~15자 사이의 문자입니다"
           />
         </div>
 
-        <button type="submit" className="btn btn-primary">아이디 찾기</button>
+        <div className="col-12 text-center">
+          <button type="submit" className="btn btn-primary">아이디 찾기</button>
+        </div>
       </form>
 
       {foundId && (
-        <div className="mt-3">
-          <p>찾은 아이디: {foundId}</p>
+        <div className="mt-4 border p-3">
+          <p className="mb-0"><strong>찾은 아이디:</strong> {foundId}</p>
+        </div>
+      )}
+
+      {noMatch && !foundId && (
+        <div className="mt-4 border p-3">
+          <p className="mb-0">일치하는 정보가 없습니다.</p>
         </div>
       )}
     </div>
