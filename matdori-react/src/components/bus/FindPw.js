915a8c30eb66f54064922de55e525410e55b2628
ph2tiger <ph2tiger@naver.com>
2023-11-17 16:19:12 +0900

    비밀번호 찾기(임시비밀번호)구현중

--- matdori-react/src/components/bus/FindPw.js
+++ matdori-react/src/components/bus/FindPw.js
@@ -1,9 +1,86 @@
-const FindPw = () => {
+import React, { useState } from "react";
+import axios from 'axios';
 
-  return(
-    <>
-    <h1>비밀번호 찾기 페이지</h1>
-    </>
+const FindPw = () => {
+  const [id, setId] = useState("");
+  const [email, setEmail] = useState("");
+  const [busRegNo, setBusRegNo] = useState("");
+  const [passwordSent, setPasswordSent] = useState(false);
+  const [noMatch, setNoMatch] = useState(false);
+
+  const handleFindPassword = (e) => {
+    e.preventDefault();
+
+    axios.post('http://localhost:8080/business/findPw', { id, email, busRegNo })
+      .then((response) => {
+        setPasswordSent(true);
+        setNoMatch(false);
+      })
+      .catch((error) => {
+        console.error('비밀번호 찾기에 실패했습니다:', error);
+        setPasswordSent(false);
+        setNoMatch(true);
+      });
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">비밀번호 찾기</h1>
+      <p className="text-center mb-4">아래 정보를 입력하시면 비밀번호를 메일로 발송해 드립니다.</p>
+      <form onSubmit={handleFindPassword} className="row g-3">
+      <div className="col-6">
+          <label htmlFor="id" className="form-label">아이디</label>
+          <input
+            type="text"
+            className="form-control"
+            id="id"
+            value={id}
+            onChange={(e) => setId(e.target.value)} 
+            required
+          />
+        </div>
+
+        <div className="col-6">
+          <label htmlFor="email" className="form-label">이메일</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="col-6">
+          <label htmlFor="busRegNo" className="form-label">사업자 등록번호</label>
+          <input
+            type="text"
+            className="form-control"
+            id="busRegNo"
+            value={busRegNo}
+            onChange={(e) => setBusRegNo(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="col-12 text-center">
+          <button type="submit" className="btn btn-primary">비밀번호 찾기</button>
+        </div>
+      </form>
+
+      {passwordSent && (
+        <div className="mt-4 border p-3">
+          <p className="mb-0">이메일로 비밀번호를 전송하였습니다.</p>
+        </div>
+      )}
+
+      {noMatch && !passwordSent && (
+        <div className="mt-4 border p-3">
+          <p className="mb-0">조회된 정보가 없습니다. 회원가입되지 않은 고객님입니다.</p>
+        </div>
+      )}
+    </div>
   );
 };
 
