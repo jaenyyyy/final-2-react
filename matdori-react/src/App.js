1a691911183c79b6745124e59d79a804dc3b15ec
TEDDDDGHCHCVBBCB <akdrjsdl102@naver.com>
2023-11-24 12:42:04 +0900

    수정작업

--- matdori-react/src/App.js
+++ matdori-react/src/App.js
@@ -18,7 +18,6 @@ import MenuByRes from './components/restaurant/menu/MenuByRes';
 import Update from './components/bus/Update';
 import BusQuit from './components/bus/BusQuit';
 import BusQuitSuccess from './components/bus/BusQuitSuccess';
-import ResAdd from './components/bus/ResAdd';
 
 
 function App() {
@@ -38,8 +37,6 @@ function App() {
               <Route path="/bus-login" element={<BusLogin/>}/>
               <Route path="/bus-profile" element={<BusProfile/>}/>
               <Route path="/bus-update" element={<Update/>}/>
-              <Route path="/bus-resAdd" element={<ResAdd/>}/>
-
               <Route path="/restaurant/menu/list" element={<MenuByRes/>}/>
               <Route path="/business/:busId/:resNo" element={<ResHome />} />
               <Route path="/business/:busId/:resNo/menu" element={<MenuByRes />} />
