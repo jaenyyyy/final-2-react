0bc2f6f2fd1c9cadc94c842d787ed605839de27f
ph2tiger <ph2tiger@naver.com>
2023-11-27 21:07:49 +0900

    매장에 공지사항 등록 구현

--- matdori-react/src/components/restaurant/notice/Notice.js
+++ matdori-react/src/components/restaurant/notice/Notice.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 const Notice = () => {
   const [notice, setNotice] = useState('');
   const [newNotice, setNewNotice] = useState('');
-  const { resNo } = useParams();
+  const [noticeLimit, setNoticeLimit] = useState(false);
+  const { resNo, busId } = useParams();
 
   useEffect(() => {
     axios.get(`http://localhost:8080/restaurant/notice/${resNo}`)
@@ -18,38 +19,62 @@ const Notice = () => {
   }, [resNo]);
 
   const handleInputChange = (e) => {
-    setNewNotice(e.target.value);
+    if (e.target.value.length <= 1000) {
+      setNewNotice(e.target.value);
+      setNoticeLimit(false);
+    } else {
+      setNoticeLimit(true);
+    }
   };
 
   const handleUpdateNotice = () => {
-    axios.put(`http://localhost:8080/restaurant/notice/${resNo}`, { newNotice })
-      .then(response => {
-        setNotice(`${newNotice}`);
-        //setNewNotice('');
-      })
-      .catch(error => {
-        console.error('Error updating notice:', error);
-      });
+    if (newNotice.trim() !== '' && !noticeLimit) {
+      axios.put(`http://localhost:8080/restaurant/notice/update/${resNo}`, { resNo, resNotice: newNotice })
+        .then(response => {
+          setNotice(newNotice);
+          setNewNotice('');
+        })
+        .catch(error => {
+          console.error('Error updating notice:', error);
+        });
+    }
   };
 
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
+          <h2>공지 등록</h2>
+
           <div className="card">
             <div className="card-body">
               <textarea
-                className="form-control mb-3"
+                className={`form-control mb-3 ${noticeLimit ? 'is-invalid' : ''}`}
                 value={newNotice}
                 onChange={handleInputChange}
+                style={{ height: '200px' }} // 입력창 높이 설정
+                placeholder='신규 공지 등록 
+                공지는 1000자 이내로 입력해주세요 '
               />
+              {noticeLimit && <div className="invalid-feedback">공지는 최대 1000자까지 등록 가능합니다.</div>}
               <button
-                className="btn btn-primary mb-3"
+                className="btn btn-primary mb-3 btn-warning"
                 onClick={handleUpdateNotice}
               >
-                공지 등록하기
+                등록하기
               </button>
-              <p>{JSON.stringify(notice)}</p>
+              <p>등록된 공지: {notice}</p>
+
+              <div className="d-flex justify-content-between align-items-end mt-3">
+                <small className="text-muted">
+                  <small>공지는 한 번에 하나만 등록할 수 있습니다.</small>
+                </small>
+                <small>
+                  <Link to={`/business/${busId}/${resNo}`} className="btn btn-primary btn-sm">
+                    매장 관리로 돌아가기
+                  </Link>
+                </small>
+              </div>
             </div>
           </div>
         </div>
@@ -57,5 +82,6 @@ const Notice = () => {
     </div>
   );
 };
+
 
 export default Notice;
