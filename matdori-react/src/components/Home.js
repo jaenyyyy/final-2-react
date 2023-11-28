94618ca7ae8aa3b8046518169a4a930608154ccb
jaenyyyy <wodms0419@naver.com>
2023-11-28 23:38:31 +0900

    commit

--- matdori-react/src/components/Home.js
+++ matdori-react/src/components/Home.js
@@ -1,18 +1,16 @@
 import { useRecoilValue } from "recoil";
 import { busIdState } from "../recoil";
+import matdoribus from '../assets/images/matdoribus.png';
 
 
-const Home = (props)=>{
+const Home = (props) => {
     const busId = useRecoilValue(busIdState);
-    
-    return(
+
+    return (
         <>
-            <h1>메인페이지</h1>
-
-            사업자 매인페이지의 모습
-
-             로그인중인 아이디 = {busId}
-
+            <div className="row">
+                {/* <img src={matdoribus} width={100} /> */}
+            </div>
         </>
     );
 
